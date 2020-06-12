998244a86ee82ac7e7f7661d4b880e6da643bdd3
Vince Williams <williamsvm20@gmail.com>
2020-06-12 12:53:47 -0400

    all character info is rendering. TODO add styling

--- starwars/src/Character.jsx
+++ starwars/src/Character.jsx
@@ -1,7 +1,17 @@
 import React from "react"
 
 const Character = (props) => {
-    return null
+    // console.log(props)
+    const {character} = props
+    // console.log(character)
+    const {name, birth_year} = character
+
+    return (
+        <div>
+            <span>{name}</span>
+            <span>{birth_year}</span>
+        </div>
+    )
 }
 
 export default Character
--- starwars/src/CharactersContainer.jsx
+++ starwars/src/CharactersContainer.jsx
@@ -2,11 +2,15 @@ import React from "react"
 import Character from "./Character"
 
 const CharactersContainer = (props) => {
-    console.log(props) //gets CharactersData -> array of 10 characters
-
+    // console.log(props) //gets charactersData -> array of 10 characters
+    const { charactersData } = props
+    // console.log(charactersData)
     return (
         <div>
-            <Character />
+            {/* mapping through charaters to create the tiles*/}
+            {charactersData.map((character, index) => {
+                return <Character key={index} character={character}/>
+            })}
         </div>
     )
 }
