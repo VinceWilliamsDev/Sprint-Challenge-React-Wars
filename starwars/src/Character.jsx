3d45074035f50de52a82c9e5a16c2893413f2605
Vince Williams <williamsvm20@gmail.com>
2020-06-12 12:26:14 -0400

    Character component has been initialized

---
+++ starwars/src/Character.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Character = (props) => {
+    return null
+}
+
+export default Character
--- starwars/src/CharactersContainer.jsx
+++ starwars/src/CharactersContainer.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import Character from "./Character"
 
 const CharactersContainer = (props) => {
-
+    console.log(props) //gets CharactersData -> array of 10 characters
 
     return (
-        null
+        <div>
+            <Character />
+        </div>
     )
 }
 
